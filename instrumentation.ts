6f939cd1f9d74131612e196d2b3895a62eb46006
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-07-19 13:50:35 +0200

    percent-encode otlp authorization header

--- instrumentation.ts
+++ instrumentation.ts
@@ -3,8 +3,10 @@ import { registerOTel } from "@vercel/otel";
 import { env } from "./env";
 
 export function register() {
+  process.env.OTEL_EXPORTER_OTLP_ENDPOINT ??= env.OTEL_EXPORTER_OTLP_ENDPOINT;
+
   if (env.OTEL_EXPORTER_OTLP_AUTHORIZATION) {
-    process.env.OTEL_EXPORTER_OTLP_HEADERS ??= `authorization=${env.OTEL_EXPORTER_OTLP_AUTHORIZATION}`;
+    process.env.OTEL_EXPORTER_OTLP_HEADERS ??= `authorization=${encodeURIComponent(env.OTEL_EXPORTER_OTLP_AUTHORIZATION)}`;
   }
 
   registerOTel({ serviceName: env.OTEL_SERVICE_NAME });
