9fadedde29c0382cec4304d3a5331d9333db291a
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-08-05 22:16:52 +0200

    fix runtime issue

--- instrumentation.ts
+++ instrumentation.ts
@@ -5,12 +5,6 @@ import { registerOTel } from "@vercel/otel";
 import { env } from "./env";
 
 export function register() {
-  process.env.OTEL_EXPORTER_OTLP_ENDPOINT ??= env.OTEL_EXPORTER_OTLP_ENDPOINT;
-
-  if (env.OTEL_EXPORTER_OTLP_AUTHORIZATION) {
-    process.env.OTEL_EXPORTER_OTLP_HEADERS ??= `authorization=${encodeURIComponent(env.OTEL_EXPORTER_OTLP_AUTHORIZATION)}`;
-  }
-
   registerOTel({
     serviceName: env.OTEL_SERVICE_NAME,
     metricReaders: [
