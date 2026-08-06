f5336dc42a8a80f69c622b8a6e4aeefad53b6bcb
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-08-06 14:31:08 +0200

    remove not needed env

--- env.ts
+++ env.ts
@@ -9,15 +9,12 @@ export const env = createEnv({
   },
   client: {
     NEXT_PUBLIC_OTEL_SERVICE_NAME: z.string(),
-    NEXT_PUBLIC_OTEL_EXPORTER_OTLP_ENDPOINT: z.string().startsWith("/"),
   },
   runtimeEnv: {
     OTEL_SERVICE_NAME: process.env.OTEL_SERVICE_NAME,
     OTEL_EXPORTER_OTLP_ENDPOINT: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
     OTEL_EXPORTER_OTLP_HEADERS: process.env.OTEL_EXPORTER_OTLP_HEADERS,
     NEXT_PUBLIC_OTEL_SERVICE_NAME: process.env.NEXT_PUBLIC_OTEL_SERVICE_NAME,
-    NEXT_PUBLIC_OTEL_EXPORTER_OTLP_ENDPOINT:
-      process.env.NEXT_PUBLIC_OTEL_EXPORTER_OTLP_ENDPOINT,
   },
   emptyStringAsUndefined: true,
 });
